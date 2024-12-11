1c82144c210269bfa406489f1fc8a210ce4b6e42
Elena8812 <Len4ik8812@gmail.com>
2024-12-11 23:59:27 +1200

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,13 @@
+immutable_var=(1, 'e', False )
+print(immutable_var)
+#immutable_var[1] = 2
+#print(immutable_var)
+#кортеж неизменяемый. не поддерживает обращение по элементам
+mutable_list=[2, 'l', True]
+print(mutable_list)
+mutable_list[0]=1,
+print(mutable_list)
+mutable_list[1]='e'
+print(mutable_list)
+mutable_list[2]= False
+print(mutable_list)
